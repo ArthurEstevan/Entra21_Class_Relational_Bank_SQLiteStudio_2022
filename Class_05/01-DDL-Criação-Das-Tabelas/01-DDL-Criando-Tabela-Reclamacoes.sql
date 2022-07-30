fcef39045f8f105e25b017470f5215f43ea513fb
ArthurEstevan <arthur.estevan01@gmail.com>
2022-07-30 01:00:23 -0300

    Organizando Readme | 30-07-22

--- Class_05/01-DDL-Criação-Das-Tabelas/01-DDL-Criando-Tabela-Reclamacoes.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE reclamacoes (
-    cliente_id  INTEGER REFERENCES cliente (id),
-    id          INTEGER PRIMARY KEY AUTOINCREMENT
-                        NOT NULL
-                        UNIQUE,
-    reclamacoes TEXT
-);
